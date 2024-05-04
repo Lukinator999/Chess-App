d2a468d68e341eb9c885682550e36c0eb20fc4f4
Lukas Schoberer <lukinator.schoberer@gmail.com>
2024-05-04 14:52:32 +0200

    Started with python connection

--- chess_app/views.py
+++ chess_app/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+import chess
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
 
 def game(request, rat):
+    board = chess.Board()
     print(rat)
+    if request.method == 'POST' and request.POST.get('is_legalmove_request', None):
+        square = request.POST.get('square')
+        square = chess.parse_square(square)
+        legal_moves = board.generate_legal_moves(square)
+        print(legal_moves)
     return render(request, "game.html")
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,21 @@
 function newGame(rating) {
     window.location.href = `/game/${rating}/`
 }
+// Funktion zum Herausfindes des Cookies für das Senden an den Server
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 function insertImage() {
     document.querySelectorAll('.box').forEach(image => {
         if (image.innerText.length !== 0) {
@@ -40,3 +55,27 @@ function coloring() {
         }
     })
 }
+let boxes = document.getElementsByClassName("box");
+boxes.forEach(box => {
+    box.addEventListener("click", function() {
+        piece = box.textContent
+        square = box.id
+        if (piece != "") {
+            let csrftoken = getCookie('csrftoken');
+            fetch('', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'X-CSRFToken': csrftoken
+                },
+                body: 'square=' + encodeURIComponent(square) + '&is_legalmoves_request=true'
+            })
+            .then(data => {
+                
+            })
+            .catch(error => {
+                reject(error);
+            })
+        }
+    });
+})
